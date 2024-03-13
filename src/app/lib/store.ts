1a2e28fec02f78a5137173b3001a7025a64bab4f
NguyenDonUET <nguyendoncb@gmail.com>
2024-03-13 09:55:19 +0700

    feat(login): Call API to retrieve user information and store it in a state.

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '@/app/lib/features/counter/counterSlice';
+import userReducer from '@/app/lib/features/user/userSlice';
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
-      counter: counterReducer
+      user: userReducer
     }
   });
 };
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,52 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../lib/hooks';
+import { UserInfoType, setIsLoggedIn } from '../lib/features/user/userSlice';
+
+const callAPI = async () => {
+  try {
+    const res = await fetch(`api/users/1`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+type GetUserInfoResponseType = {
+  message: string;
+  userInfo: {
+    email: string;
+  };
+};
+
 export default function UserPage() {
-  return <div> UserPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { userInfo } = useAppSelector((state) => {
+    return state.user;
+  });
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    // fake loading time
+    setTimeout(() => {
+      callAPI().then((result) => {
+        const { userInfo } = result as GetUserInfoResponseType;
+        dispatch(setIsLoggedIn(userInfo));
+      });
+
+      setIsLoading(false);
+    }, 3000);
+    return () => {};
+  }, []);
+  return (
+    <div>
+      <h1>UserPage</h1>
+      {isLoading ? <h2>Loading...</h2> : <h3>Email: {userInfo.email}</h3>}
+    </div>
+  );
 }
