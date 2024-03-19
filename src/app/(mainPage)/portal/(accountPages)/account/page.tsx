61d8b80ac6be1db9c851a57b6a82fdc6f26d9ad6
NguyenDonUET <nguyendoncb@gmail.com>
2024-03-19 16:32:05 +0700

    feat: implement EditUserPassword in accountPage page

--- src/app/(mainPage)/portal/(accountPages)/account/page.tsx
+++ src/app/(mainPage)/portal/(accountPages)/account/page.tsx
@@ -1,3 +1,11 @@
+import EditUserPassword from '@/components/portal/account/EditUserPassword';
+import { Stack } from '@mui/material';
+
 export default function AccountPage() {
-  return <div> AccountPage</div>;
+  return (
+    <Stack spacing={2}>
+      <h2>EditPersonalInfo</h2>
+      <EditUserPassword />
+    </Stack>
+  );
 }
