65e6d534bc0b4471488fe8c2fa1eb6034e260760
NguyenDonUET <nguyendoncb@gmail.com>
2024-03-21 02:15:50 +0700

    feat: add modal to create new vaccination point

--- src/app/(mainPage)/admin/vaccination-point/page.tsx
+++ src/app/(mainPage)/admin/vaccination-point/page.tsx
@@ -1,12 +1,34 @@
+'use client';
 import AdminVaccinePointsTable from '@/components/admin/vaccine-points/AdminVaccinePointsTable';
+import CreateModal from '@/components/admin/vaccine-points/CreateModal';
 import SearchSection from '@/components/admin/vaccine-points/SearchSection';
-import { Stack } from '@mui/material';
+import { Button, Stack } from '@mui/material';
+import { useState } from 'react';
 
 export default function VaccinationPointPage() {
+  const [createModal, setCreateModalOpen] = useState<boolean>(false);
+
+  const handleOpenCreateModal = () => {
+    setCreateModalOpen(true);
+  };
+
+  const handleCloseCreateModal = () => {
+    setCreateModalOpen(false);
+  };
+
   return (
     <Stack spacing={2}>
-      <SearchSection />
+      <Stack spacing={2} direction={'row'}>
+        <SearchSection />
+        <Button variant="contained" onClick={() => handleOpenCreateModal()}>
+          Tạo mới
+        </Button>
+      </Stack>
       <AdminVaccinePointsTable />
+      <CreateModal
+        createModalOpen={createModal}
+        handleCloseCreateModal={handleCloseCreateModal}
+      />
     </Stack>
   );
 }
