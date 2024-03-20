cce23a88ccb21dc54b3705c49b40b468dd0f6926
NguyenDonUET <nguyendoncb@gmail.com>
2024-03-21 03:32:51 +0700

    feat: create vaccineRegistrationResultSlice

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '@/app/lib/features/user/userSlice';
 import vaccinationPointReducer from '@/app/lib/features/vaccinationPoint/vaccinationPointSlice';
 import vaccineRegistrationReducer from '@/app/lib/features/vaccineRegistration/vaccineRegistrationSlice';
+import vaccineRegistrationResultReducer from '@/app/lib/features/vaccineRegistrationResult/vaccineRegistrationResultSlice';
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       user: userReducer,
       vaccinationPoint: vaccinationPointReducer,
-      vaccineRegistration: vaccineRegistrationReducer
+      vaccineRegistration: vaccineRegistrationReducer,
+      vaccineRegistrationResult: vaccineRegistrationResultReducer
     }
   });
 };
