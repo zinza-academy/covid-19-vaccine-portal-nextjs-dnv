93ed84dbd1fadb131fbaad000d1d0b45b1411d74
NguyenDonUET <nguyendoncb@gmail.com>
2024-03-21 11:39:27 +0700

    feat(ui): create vaccine registration table and pagination

--- src/app/(mainPage)/admin/vaccination-registration-result/page.tsx
+++ src/app/(mainPage)/admin/vaccination-registration-result/page.tsx
@@ -1,10 +1,12 @@
 import SearchSection from '@/components/admin/vaccineRegistration/SearchSection';
+import VaccineRegistrationTable from '@/components/admin/vaccineRegistration/VaccineRegistrationTable';
 import { Stack } from '@mui/material';
 
 export default function VaccinationRegistrationResultPage() {
   return (
     <Stack spacing={2}>
       <SearchSection />
+      <VaccineRegistrationTable />
     </Stack>
   );
 }
