138fe04b10208e78a36d488f1b217d32c524d9d5
NguyenDonUET <nguyendoncb@gmail.com>
2024-03-25 23:09:37 +0700

    feat(document): create add document modal and handle validation

--- src/app/(mainPage)/admin/document/page.tsx
+++ src/app/(mainPage)/admin/document/page.tsx
@@ -1,10 +1,31 @@
+'use client';
+import CreateModal from '@/components/admin/document/CreateModal';
 import DocumentTable from '@/components/admin/document/DocumentTable';
-import { Box } from '@mui/material';
+import { Button, Stack } from '@mui/material';
+import React from 'react';
 
 export default function AdminDocumentPage() {
+  const [createModalOpen, setCreateModalOpen] = React.useState<boolean>(false);
+
+  const handleOpenCreateModal = () => {
+    setCreateModalOpen(true);
+  };
+
+  const handleCloseCreateModal = () => {
+    setCreateModalOpen(false);
+  };
   return (
-    <Box>
+    <Stack spacing={2}>
+      <Stack alignItems="end">
+        <Button variant="contained" onClick={handleOpenCreateModal}>
+          Thêm mới
+        </Button>
+        <CreateModal
+          handleCloseCreateModal={handleCloseCreateModal}
+          createModalOpen={createModalOpen}
+        />
+      </Stack>
       <DocumentTable />
-    </Box>
+    </Stack>
   );
 }
